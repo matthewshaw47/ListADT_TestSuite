272c74fce88f4d4e386b8b7550930a87a2246c75
Matt Shaw <matthewshaw47@hotmail.com>
2017-12-02 03:22:35 -0600

    commit

--- Project4/src/EmptyQueueException.java
+++
@@ -1,9 +0,0 @@
-/**
- * Custom exception type for empty queues
- */
-public class EmptyQueueException extends RuntimeException {
-	public EmptyQueueException() { super(); }
-	public EmptyQueueException(String msg) {
-		super(msg);
-	}
-}
